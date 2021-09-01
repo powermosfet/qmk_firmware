c4cacf4a672f498ac7f10e9cc43a7a7f12c41f62
Asmund Berge <herrasbe@gmail.com>
2021-09-01 21:29:16 +0200

    Vove modifiers around

--- keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
+++ keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
@@ -87,9 +87,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_D,     KC_R,     KC_W,      KC_B,               KC_J,     KC_F,     KC_U,     KC_P,     XXXXXXX,  KC_BSPC,
-    KC_ESC,   KC_A,     KC_S,     KC_H,     KC_T,      KC_G,               KC_Y,     KC_N,     KC_E,     KC_O,     KC_I,     KC_ENT,  
-    XXXXXXX,  KC_Z,     KC_X,     KC_M,     KC_C,      KC_V,               KC_K,     KC_L,     KC_COMM,  KC_DOT,   KC_SLSH,  XXXXXXX,
-                                            KC_LCTL,   SYMBOLS,  SPACE,    ARROWS,   KC_RGUI
+    KC_ESC,   KC_A,     KC_S,     KC_H,     KC_T,      KC_G,               KC_Y,     KC_N,     KC_E,     KC_O,     KC_I,     KC_ENT,
+    KC_LALT,  KC_Z,     KC_X,     KC_M,     KC_C,      KC_V,               KC_K,     KC_L,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RALT,
+                                            KC_RGUI,   SYMBOLS,  SPACE,    ARROWS,   KC_LCTL
   ),
   
   [_SYMBOLS] = LAYOUT_reviung41(

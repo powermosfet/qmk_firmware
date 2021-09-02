be9600868a6bb8902932c8815b298e27acbf410d
Asmund Berge <herrasbe@gmail.com>
2021-09-02 14:24:19 +0200

    Layer colors, and a dim white for the base layer

--- keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
+++ keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
@@ -69,6 +69,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 };
 
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 11, 0, 0, 50}
+);
+const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {10, 1, HSV_CYAN}
+);
+const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {10, 1, HSV_PURPLE}
+);
+const rgblight_segment_t PROGMEM my_layer4_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {10, 1, HSV_YELLOW}
+);
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_layer1_layer,
+    my_layer2_layer,
+    my_layer3_layer,
+    my_layer4_layer
+);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_D,     KC_R,     KC_W,      KC_B,               KC_J,     KC_F,     KC_U,     KC_P,     XXXXXXX,  KC_BSPC,
@@ -81,14 +101,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_HASH,  KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,   KC_GRAVE,           KC_QUOT,  KC_7,     KC_8,     KC_9,     KC_PAST,  _______,
     KC_SCLN,  KC_AMPR,  KC_DLR,   KC_LPRN,  KC_RPRN,   KC_PIPE,            KC_COLN,  KC_4,     KC_5,     KC_6,     KC_PPLS,  KC_MINS,
     _______,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,   KC_TILD,            KC_DQUO,  KC_1,     KC_2,     KC_3,     _______,  KC_EQL,
-                                            _______,   _______,  _______,  FN,       KC_0  
+                                            _______,   _______,  _______,  _______,  KC_0  
   ),
   
   [_ARROWS] = LAYOUT_reviung41(
     _______,  _______,  _______,  _______,  _______,   _______,            KC_HOME,  KC_PGUP,  KC_UP,    KC_PGDN,  KC_END,   KC_VOLU,
     _______,  _______,  _______,  _______,  _______,   _______,            KC_INS,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_DEL,   KC_VOLD,
     _______,  _______,  _______,  _______,  _______,   _______,            _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  KC_MUTE,
-                                            _______,   FN,       _______,  _______,  _______
+                                            _______,   _______,  _______,  _______,  _______
   ),
   
   [_FN] = LAYOUT_reviung41(
@@ -99,7 +119,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _SYMBOLS, _ARROWS, _FN);
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
 }
+
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(0, layer_state_cmp(state, _BASE));
+
+    return state;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    state = update_tri_layer_state(state, _SYMBOLS, _ARROWS, _FN);
+
+    rgblight_set_layer_state(1, layer_state_cmp(state, _SYMBOLS));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _ARROWS));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _FN));
+
+    return state;
+}
+
 

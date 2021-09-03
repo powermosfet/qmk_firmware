77e1cde4943939d6c54703cbff453acc31acb4d3
Asmund Berge <herrasbe@gmail.com>
2021-09-03 14:33:22 +0200

    Duplicate layer keys

--- keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
+++ keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     G_ESC,    KC_A,     KC_S,     KC_H,     KC_T,      KC_G,               KC_Y,     KC_N,     KC_E,     KC_O,     KC_I,     G_ENT,  
     KC_LCTL,  KC_Z,     KC_X,     KC_M,     KC_C,      KC_V,               KC_K,     KC_L,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RCTL,
 
-                                  KC_TAB,         SYMBOLS,       SPACE,         ARROWS,        XXXXXXX
+                                  ARROWS,         SYMBOLS,       SPACE,         ARROWS,        SYMBOLS
   ),
   
   [_SYMBOLS] = LAYOUT_reviung41(

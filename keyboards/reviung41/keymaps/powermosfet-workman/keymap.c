48c19067bb53535fe2d9417906526e2f2731a08e
Asmund Berge <herrasbe@gmail.com>
2021-09-03 10:08:34 +0200

    Power button

--- keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
+++ keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
@@ -94,28 +94,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_D,     KC_R,     KC_W,      KC_B,               KC_J,     KC_F,     KC_U,     KC_P,     XXXXXXX,  KC_BSPC,
     KC_ESC,   KC_A,     KC_S,     KC_H,     KC_T,      KC_G,               KC_Y,     KC_N,     KC_E,     KC_O,     KC_I,     KC_ENT,
     KC_LALT,  KC_Z,     KC_X,     KC_M,     KC_C,      KC_V,               KC_K,     KC_L,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RALT,
-                                            KC_RGUI,   SYMBOLS,  SPACE,    ARROWS,   KC_LCTL
+
+                                  KC_RGUI,        SYMBOLS,       SPACE,         ARROWS,        KC_LCTL
   ),
   
   [_SYMBOLS] = LAYOUT_reviung41(
     KC_HASH,  KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,   KC_GRAVE,           KC_QUOT,  KC_7,     KC_8,     KC_9,     KC_PAST,  _______,
     KC_SCLN,  KC_AMPR,  KC_DLR,   KC_LPRN,  KC_RPRN,   KC_PIPE,            KC_COLN,  KC_4,     KC_5,     KC_6,     KC_PPLS,  KC_MINS,
     _______,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,   KC_TILD,            KC_DQUO,  KC_1,     KC_2,     KC_3,     _______,  KC_EQL,
-                                            _______,   _______,  _______,  _______,  KC_0  
+
+                                  _______,        _______,       _______,       _______,       KC_0  
   ),
   
   [_ARROWS] = LAYOUT_reviung41(
     _______,  _______,  _______,  LEFT,     RIGHT,     _______,            KC_HOME,  KC_PGUP,  KC_UP,    KC_PGDN,  KC_END,   KC_VOLU,
     _______,  _______,  _______,  LPIZZA,   RPIZZA,    _______,            KC_INS,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_DEL,   KC_VOLD,
     _______,  _______,  _______,  LBIND,    RBIND,     _______,            _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  KC_MUTE,
-                                            _______,   _______,  _______,  _______,  _______
+
+                                  _______,        _______,       _______,       _______,       _______
   ),
   
   [_FN] = LAYOUT_reviung41(
-    _______,  _______,  _______,  _______,  _______,   _______,            _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11, 
+    KC_PWR,   _______,  _______,  _______,  _______,   _______,            _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11, 
     _______,  _______,  _______,  _______,  _______,   _______,            _______,  KC_F4,    KC_F5,    KC_F6,    KC_F12,   KC_F13, 
     _______,  _______,  _______,  _______,  _______,   _______,            _______,  KC_F1,    KC_F2,    KC_F3,    KC_BSLS,  _______,
-                                            _______,   _______,  _______,  _______,  _______
+
+                                  _______,       _______,        _______,       _______,       _______
   ),
 };
 

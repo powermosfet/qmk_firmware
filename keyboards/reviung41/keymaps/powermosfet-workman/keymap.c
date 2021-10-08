cbb5104250e3bd293940329c5b84624ab56daca9
Asmund Berge <herrasbe@gmail.com>
2021-10-08 10:48:50 +0200

    Separate nordiv and tmux layers

--- keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
+++ keyboards/reviung41/keymaps/powermosfet-workman/keymap.c
@@ -17,25 +17,29 @@
 
 enum layer_names {
     _BASE,
+    _NOR,
+    _TMUX,
     _SYMBOLS,
     _ARROWS,
     _FN
 };
 
 #define SYMBOLS  MO(_SYMBOLS)
-#define ARROWS  MO(_ARROWS)
-#define FN MO(_FN)
-#define S_SYMBOLS  LM(_SYMBOLS, MOD_LSFT)
-#define S_ARROWS  LM(_ARROWS, MOD_LSFT)
+#define ARROWS   MO(_ARROWS)
+#define FN       MO(_FN)
+#define NOR      MO(_NOR)
+#define TMUX     MO(_TMUX)
 
-#define SPACE  LSFT_T(KC_SPACE)
-#define A_TAB  LALT_T(KC_TAB)
-#define G_ESC  LGUI_T(KC_ESCAPE)
-#define A_BSPC RALT_T(KC_BSPACE)
-#define G_ENT  RGUI_T(KC_ENTER)
-#define NOR_AA RALT(KC_W)
-#define NOR_OE RALT(KC_L)
-#define NOR_AE RALT(KC_Z)
+#define SPACE   LSFT_T(KC_SPACE)
+#define A_TAB   LALT_T(KC_TAB)
+#define G_ESC   LGUI_T(KC_ESCAPE)
+#define A_BSPC  RALT_T(KC_BSPACE)
+#define G_ENT   RGUI_T(KC_ENTER)
+#define NOR_AA  RALT(KC_W)
+#define NOR_OE  RALT(KC_L)
+#define NOR_AE  RALT(KC_Z)
+#define SH_Z    LSFT_T(KC_Z)
+#define SH_SLSH LSFT_T(KC_SLSH)
 
 enum custom_keycodes {
     LEFT = SAFE_RANGE,
@@ -44,6 +48,17 @@ enum custom_keycodes {
     RPIZZA,
     LBIND,
     RBIND,
+    TMUX_J,
+    TMUX_P,
+    TMUX_A,
+    TMUX_S,
+    TMUX_H,
+    TMUX_N,
+    TMUX_O,
+    TMUX_K,
+    TMUX_L,
+    TMUX_C,
+    TMUX_X,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -72,39 +87,107 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case LBIND:
             SEND_STRING(">>=");
             break;
+
+        case TMUX_J:
+            SEND_STRING(SS_LCTL("a") "j");
+            break;
+
+        case TMUX_P:
+            SEND_STRING(SS_LCTL("a") "p");
+            break;
+
+        case TMUX_A:
+            SEND_STRING(SS_LCTL("a") "a");
+            break;
+
+        case TMUX_S:
+            SEND_STRING(SS_LCTL("a") "s");
+            break;
+
+        case TMUX_H:
+            SEND_STRING(SS_LCTL("a") "h");
+            break;
+
+        case TMUX_N:
+            SEND_STRING(SS_LCTL("a") "n");
+            break;
+
+        case TMUX_O:
+            SEND_STRING(SS_LCTL("a") "o");
+            break;
+
+        case TMUX_K:
+            SEND_STRING(SS_LCTL("a") "k");
+            break;
+
+        case TMUX_L:
+            SEND_STRING(SS_LCTL("a") "l");
+            break;
+
+        case TMUX_C:
+            SEND_STRING(SS_LCTL("a") "c");
+            break;
+
+        case TMUX_X:
+            SEND_STRING(SS_LCTL("a") "x");
+            break;
         }
     }
 
     return true;
 };
 
-const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+const rgblight_segment_t PROGMEM rgb_base[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 11, 0, 0, 50}
 );
-const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+const rgblight_segment_t PROGMEM rgb_nor[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 11, 0, 0, 50}
+);
+const rgblight_segment_t PROGMEM rgb_tmux[] = RGBLIGHT_LAYER_SEGMENTS(
+    {10, 1, HSV_BLUE}
+);
+const rgblight_segment_t PROGMEM rgb_symbols[] = RGBLIGHT_LAYER_SEGMENTS(
     {10, 1, HSV_CYAN}
 );
-const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+const rgblight_segment_t PROGMEM rgb_arrows[] = RGBLIGHT_LAYER_SEGMENTS(
     {10, 1, HSV_PURPLE}
 );
-const rgblight_segment_t PROGMEM my_layer4_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+const rgblight_segment_t PROGMEM rgb_fn[] = RGBLIGHT_LAYER_SEGMENTS(
     {10, 1, HSV_YELLOW}
 );
 
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_layer1_layer,
-    my_layer2_layer,
-    my_layer3_layer,
-    my_layer4_layer
+const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    rgb_base,
+    rgb_nor,
+    rgb_tmux,
+    rgb_symbols,
+    rgb_arrows,
+    rgb_fn
 );
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
-    A_TAB,    KC_Q,     KC_D,     KC_R,     KC_W,      KC_B,               KC_J,     KC_F,     KC_U,     KC_P,     XXXXXXX,  A_BSPC,  
+    A_TAB,    KC_Q,     KC_D,     KC_R,     KC_W,      KC_B,               KC_J,     KC_F,     KC_U,     KC_P,     NOR,      A_BSPC,  
     G_ESC,    KC_A,     KC_S,     KC_H,     KC_T,      KC_G,               KC_Y,     KC_N,     KC_E,     KC_O,     KC_I,     G_ENT,  
-    KC_LCTL,  KC_Z,     KC_X,     KC_M,     KC_C,      KC_V,               KC_K,     KC_L,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RCTL,
+    KC_LCTL,  SH_Z,     KC_X,     KC_M,     KC_C,      KC_V,               KC_K,     KC_L,     KC_COMM,  KC_DOT,   SH_SLSH,  KC_RCTL,
 
-                                  S_SYMBOLS,      SYMBOLS,       SPACE,         ARROWS,        S_ARROWS 
+                                  XXXXXXX,        SYMBOLS,       KC_SPACE,      ARROWS,        TMUX    
+  ),
+  
+  [_NOR] = LAYOUT_reviung41(
+    _______,  _______,  _______,  _______,  _______,   _______,            _______,  NOR_OE,   NOR_AA,   NOR_AE,   _______,  _______,
+    _______,  _______,  _______,  _______,  _______,   _______,            _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,   _______,            _______,  _______,  _______,  _______,  _______,  _______,
+
+                                  _______,       _______,        _______,       _______,       _______
+  ),
+  
+  [_TMUX] = LAYOUT_reviung41(
+    _______,  _______,  _______,  _______,  _______,   _______,            TMUX_J,   _______,  _______,  TMUX_P,   _______,  _______,
+    _______,  TMUX_A,   TMUX_S,   TMUX_H,   _______,   _______,            _______,  TMUX_N,   _______,  TMUX_O,   _______,  _______,
+    _______,  _______,  TMUX_X,   _______,  TMUX_C,    _______,            TMUX_K,   TMUX_L,   _______,  _______,  _______,  _______,
+
+                                  _______,       _______,        _______,       _______,       _______
   ),
   
   [_SYMBOLS] = LAYOUT_reviung41(
@@ -116,16 +199,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   
   [_ARROWS] = LAYOUT_reviung41(
-    _______,  _______,  NOR_AA,   LEFT,     RIGHT,     _______,            KC_HOME,  KC_PGUP,  KC_UP,    KC_PGDN,  KC_END,   KC_VOLU,
-    _______,  _______,  NOR_OE,   LPIZZA,   RPIZZA,    _______,            KC_INS,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_DEL,   KC_VOLD,
-    _______,  _______,  NOR_AE,   LBIND,    RBIND,     _______,            _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  KC_MUTE,
+    _______,  _______,  _______,  LEFT,     RIGHT,     _______,            KC_HOME,  KC_PGUP,  KC_UP,    KC_PGDN,  KC_END,   KC_VOLU,
+    _______,  _______,  _______,  LPIZZA,   RPIZZA,    _______,            KC_INS,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_DEL,   KC_VOLD,
+    _______,  _______,  _______,  LBIND,    RBIND,     _______,            _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  KC_MUTE,
 
                                   _______,        _______,       KC_TAB,        _______,       _______
   ),
   
   [_FN] = LAYOUT_reviung41(
-    KC_PWR,   _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,   _______,            _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11, 
-    _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,   _______,            _______,  KC_F4,    KC_F5,    KC_F6,    KC_F12,   KC_F13, 
+    KC_PWR,   _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,   KC_WH_U,            _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11, 
+    _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,   KC_WH_D,            _______,  KC_F4,    KC_F5,    KC_F6,    KC_F12,   KC_F13, 
     _______,  _______,  _______,  _______,  _______,   _______,            _______,  KC_F1,    KC_F2,    KC_F3,    KC_BSLS,  _______,
 
                                   _______,       _______,        _______,       _______,       _______
@@ -134,7 +217,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void keyboard_post_init_user(void) {
     // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
+    rgblight_layers = rgb_layers;
 }
 
 layer_state_t default_layer_state_set_user(layer_state_t state) {
